f0bba71bf7c497e380d8e7643dfc6f6ba5386bd3
Andriy <grushevoyandrey@gmail.com>
2025-06-02 18:41:58 +0100

    add script to fetch weather in Paris and save selected data to JSON

--- app/main.py
+++ app/main.py
@@ -1,6 +1,19 @@
+import json
+import requests
+
+
 def get_weather() -> None:
-    # write your code here
-    pass
+    url = "https://api.weatherapi.com/v1/current.json?key=d1fe3b5d476a4b0494d155040250206&q=Paris"
+    res = requests.get(url)
+    data = res.json()
+
+    its_necessary_data = {
+        "Country": data["location"]["country"],
+        "Time&Data": data["location"]["localtime"],
+        "Temperature": data["current"]["temp_c"],
+    }
+    with open("weather.json", "w", encoding="utf-8") as f:
+        json.dump(its_necessary_data, f, ensure_ascii=False, indent=2)
 
 
 if __name__ == "__main__":
